d8b2ea681f2d25b9b760d14148ca5674df47e2b0
Pete Snyder <cybernexus00@yahoo.com>
2025-03-21 14:08:20 -0400

    [FIX] scrap_reason_code: resolves issue where scrap location gets changed to default value due to being readonly

--- scrap_reason_code/models/stock_scrap.py
+++ scrap_reason_code/models/stock_scrap.py
@@ -18,8 +18,9 @@ class StockScrap(models.Model):
         comodel_name="scrap.reason.code",
         compute="_compute_allowed_reason_code_ids",
     )
-    scrap_location_id = fields.Many2one(readonly=True)
 
+    scrap_location_id = fields.Char(compute='_compute_scrap_location_id', stored=True, readonly=True)
+    
     @api.depends("product_id", "product_id.categ_id")
     def _compute_allowed_reason_code_ids(self):
         for rec in self:
@@ -53,27 +54,8 @@ class StockScrap(models.Model):
         res["reason_code_id"] = self.reason_code_id.id
         return res
 
-    @api.onchange("reason_code_id")
-    def _onchange_reason_code_id(self):
-        if self.reason_code_id.location_id:
-            self.scrap_location_id = self.reason_code_id.location_id
+    @api.depends('reason_code_id')
+    def _compute_scrap_location_id(self):
+        for scrap in self:
+            scrap.scrap_location_id = scrap.reason_code_id.location_id.name
 
-    def _update_scrap_reason_code_location(self, vals):
-        if "reason_code_id" in vals:
-            location_id = (
-                self.env["scrap.reason.code"]
-                .browse(vals.get("reason_code_id"))
-                .location_id.id
-            )
-            if location_id:
-                vals.update({"scrap_location_id": location_id})
-
-    def write(self, vals):
-        self._update_scrap_reason_code_location(vals)
-        return super().write(vals)
-
-    @api.model_create_multi
-    def create(self, vals_list):
-        for vals in vals_list:
-            self._update_scrap_reason_code_location(vals)
-        return super().create(vals_list)
